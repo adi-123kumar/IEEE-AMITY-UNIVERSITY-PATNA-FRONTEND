eccf4a9bb50381f681a98c314f85f4c75b1a15b1
aditya kumar <adityarishu134@gmail.com>
2025-08-04 15:47:54 +0530

    first commit

---
+++ js/team.js
@@ -0,0 +1,13 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray("section").forEach(section => {
+  gsap.from(section, {
+    opacity: 0,
+    y: 50,
+    duration: 1,
+    scrollTrigger: {
+      trigger: section,
+      start: "top 80%",
+    }
+  });
+});
